5b80eb176c234a7ce78b16a09f9eab7d9ba6786c
Diederik <diederik1905@gmail.com>
2024-03-03 16:54:09 +0100

    working on ai

--- src/connectfour/core.clj
+++ src/connectfour/core.clj
@@ -1,6 +1,7 @@
 (ns connectfour.core)
 
 (def field ["-", "X", "O"])
+(def ai 3)
 
 (def empty-board
   "Empty board 7x6"
@@ -23,6 +24,16 @@
       (println row)))
   (println (range 0 7) "\n"))
 
+(defn check-four-in-a-row
+  "Check if there are four in a row for a given player."
+  [list player]
+  (boolean (let [sublist (for [i (range (- (count list) 3))
+                      :let [k (nth list i) l (nth list (+ i 1)) m (nth list (+ i 2)) n (nth list (+ i 3))]]
+                  (if (every? #(= % (field player)) [k l m n])
+                    true
+                    false))]
+    (some true? sublist))))
+
 (defn win-vertically?
   "Check if the player has won the game vertically (a row of the board vector)."
   [board x y player]
@@ -44,7 +55,7 @@
 (defn win-diagonally?
   "Check if the player has won the game diagonally from top left to bottom right."
   [board x y player direction]
-  (let [diag (for [i (range -6 6)
+  (let [diag (for [i (range -6 7)
                    :let [x' (+ x i) y' (direction y i)]
                    :when (and (>= x' 0) (< x' 7) (>= y' 0) (< y' 6))]
               (get-in board [x' y']))]
@@ -66,6 +77,74 @@
             (win-diagonally? board x y player +)
             (win-diagonally? board x y player -))))
 
+(defn draw?
+  "Check if the game is a draw."
+  [board]
+  (every? #(not= (field 0) %) (flatten board)))
+
+(defn get-possible-locations
+  "Get all possible locations for the AI to move."
+  [board]
+  (let [locations (for [x (range 7)
+                        :let [y (get-top-y board x)]
+                        :when (not= y 6)]
+                    [x y])]
+    (shuffle locations)))
+
+(defn score-window
+  "Score a window for the AI."
+  [window player]
+  (let [opponent (if (= player 1) 2 1)]
+    (cond
+      (every? #(= % (field player)) window) 100,
+      (and (count (filter #(= % (field player)) window)) 3 (count (filter #(= % (field 0)) window)) 1) 5,
+      (and (count (filter #(= % (field player)) window)) 2 (count (filter #(= % (field 0)) window)) 2) 2,
+      (and (count (filter #(= % (field opponent)) window)) 3 (count (filter #(= % (field 0)) window)) 1) -4)))
+
+
+(defn score-board
+  "Score the board for the AI."
+  [board player]
+  ;; make all possible windows of 4
+  
+  )
+
+(defn minimax
+  "AI algorithm to determine the best move."
+  [board x y player depth maximising-player]
+  (let [locations (get-possible-locations board)]
+    (println locations)
+    (if (or (= depth 0) (win? board x y player) (win? board x y (if (= player 1) 2 1)) (draw? board))
+      (if (= depth 0)
+        (score-board board player)
+        (if (win? board x y player)
+          (if (= player 1)
+            (x -999999)
+            (x 999999))
+          (if (win? board x y (if (= player 1) 2 1))
+            (if (= player 2)
+              (x 999999)
+              (x -999999))
+            (x 0))))
+      (if maximising-player
+        (let [best-value -999999]
+          (doseq [location locations]
+            (let [value (minimax (insert board (first location) (second location) player) (first location) (second location) player (- depth 1) false)] 
+              (set! best-value (max best-value (second value)))
+              (println best-value)))
+          best-value)
+        (let [best-value 999999]
+          (doseq [location locations]
+            (let [value (minimax (insert board (first location) (second location) player) (first location) (second location) player (- depth 1) true)]
+              (set! best-value (min best-value (second value)))
+              (println best-value)))
+          best-value)))))
+
+(defn get-best-move
+  "Get the best move for the AI."
+  [board player]
+  (let [x (minimax board 0 0 player ai true) y (get-top-y board x)]
+    [x y]))
 
 (defn -main
   []
@@ -75,19 +154,35 @@
     (print-board @board)
 
     (loop [player 1]
-      (println (str "Player " player ", please enter a column number: "))
-      (let [x (read) y (get-top-y @board x)]
-        (if (or (< x 0) (> x 6) (every? #(not= (field 0) %) (get-in @board [x])))
+      (if (draw? @board)
+        (do
+          (println "It's a draw!")
+          (System/exit 0))
+        (if (= player 1)
           (do
-            (println "Invalid column number. Please try again.")
-            (recur player))
+            (println (str "Player " player ", please enter a column number: "))
+            (let [x (read) y (get-top-y @board x)]
+              (if (or (< x 0) (> x 6))
+                (do
+                  (println "Invalid column number. Please try again.")
+                  (recur player))
+                (do
+                  (swap! board #(insert % x y player))
+                  (print-board @board)
+                  (if (win? @board x y player)
+                    (do
+                      (println (str "Player " player " wins!"))
+                      (System/exit 0))
+                    (recur (if (= player 1) 2 1)))))))
           (do
-            (swap! board #(insert % x y player))
-            (print-board @board)
-            (if (win? @board x y player)
-              (do
-                (println (str "Player " player " wins!"))
-                (System/exit 0))
-              (recur (if (= player 1) 2 1)))))))))
+            (println "AI is thinking...")
+            (let [[x y] (get-best-move @board player)]
+                  (swap! board #(insert % x y player))
+                  (print-board @board)
+                  (if (win? @board x y player)
+                    (do
+                      (println (str "Player " player " wins!"))
+                      (System/exit 0))
+                    (recur (if (= player 1) 2 1))))))))))
 
 (-main)
